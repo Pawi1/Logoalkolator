49b7c8d52fb231f8173a6f926d282687b4f32f94
Pawi1 <p.rutkow@o2.pl>
2023-10-31 09:58:12 +0100

    Poprawnienie systemu logowania, sprawdzania trzeźwości dodanie systemu liczenia prób

--- Auth.cs
+++ Auth.cs
@@ -1,5 +1,4 @@
 using System.Globalization;
-
 namespace Logoalkolator;
 public class Auth
 {
@@ -7,6 +6,7 @@ public class Auth
     static bool isSober;
     static bool isCorrectLogin;
     static string path = AppDomain.CurrentDomain.BaseDirectory;
+
     static public bool tryAuth()
     {
         if (tryLogin())
@@ -16,13 +16,14 @@ public class Auth
                 isAuth = true;
             }
         }
+
         return isAuth;
     }
     static bool tryLogin()
     {
         List<string> Users = new List<string>();
         List<string> Passwords = new List<string>();
-        using (StreamReader sr = new StreamReader(path+"db.txt"))
+        using (StreamReader sr = new StreamReader(path + "db.txt"))
         {
             while (!sr.EndOfStream)
             {
@@ -32,6 +33,7 @@ public class Auth
                 Passwords.Add(Temp[1]);
             }
         }
+
         for (int j = 0; j < 10; j++)
         {
             Console.WriteLine("\tAutoryzacja");
@@ -41,35 +43,59 @@ public class Auth
             string userPassword = Console.ReadLine() ?? "";
             for (int i = 0; i < Users.Count; i++)
             {
-                if(userLogin == Users[i] && userPassword == Passwords[i]){Console.Write("Zostały podane poprawne dane, możesz się zalogować"); isCorrectLogin = true; break;}
+                if (userLogin == Users[i] && userPassword == Passwords[i])
+                {
+                    Console.Write("Zostały podane poprawne dane, możesz się zalogować");
+                    isCorrectLogin = true;
+                    break;
+                }
             }
-            if(!isCorrectLogin)Console.Write("Podanie dane są niepropawne");
+
+            if (!isCorrectLogin) Console.Write($"Podanie dane są niepropawne, pozostało {10 - j - 1} prób ...");
             Thread.Sleep(1500);
             Console.Clear();
-            if(isCorrectLogin)break;
+            if (isCorrectLogin) break;
         }
+
         return isCorrectLogin;
     }
     static bool Logalkolator()
     {
-        Random random = new Random();
-        Console.WriteLine("\tPróba trzeźwości! Oto zadanie:");
-        string exercise;
-        using (StreamReader sr = new StreamReader(path + "examples.txt"))
+        for (int j = 0; j < 2; j++)
         {
-            int i = 0;
-            List<string> Examp = new List<string>();
-            while (!sr.EndOfStream){string line = sr.ReadLine()??"";i++;Examp.Add(line);}
-            exercise = Examp[random.Next(0, i)];
+            Random random = new Random();
+            Console.WriteLine("\tPróba trzeźwości! Oto zadanie:");
+            string exercise;
+            using (StreamReader sr = new StreamReader(path + "examples.txt"))
+            {
+                int i = 0;
+                List<string> Examp = new List<string>();
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine() ?? "";
+                    i++;
+                    Examp.Add(line);
+                }
+
+                exercise = Examp[random.Next(0, i)];
+            }
+            Console.Write(exercise + " = ");
+            string userAnswer = Console.ReadLine() ?? "";
+            string correctAnswer =
+                Convert.ToString(Calc.CalculateRPN(Calc.ConvertToRPN(Calc.expressionToList(exercise))),
+                    CultureInfo.CurrentCulture);
+            Console.WriteLine($"Poprawna odpowiedź: {correctAnswer}\nTwoja odpowiedź: {userAnswer}");
+            if (userAnswer == correctAnswer)
+            {
+                Console.Write("Przyznano dostęp");
+                isSober = true;
+            }
+            else if (j == 0) Console.Write($"Odmówiono dostępu, masz jeszcze drugą szanse ...");
+            else Console.Write($"Odmówiono dostępu, program wyłączy się ...");
+
+            Thread.Sleep(1500);
+            Console.Clear();
         }
-        Console.Write(exercise + " = ");
-        string userAnswer = Console.ReadLine() ?? "";
-        string correctAnswer = Convert.ToString(Calc.CalculateRPN(Calc.ConvertToRPN(Calc.expressionToList(exercise))), CultureInfo.CurrentCulture);
-        Console.WriteLine($"Poprawna odpowiedź: {correctAnswer}\nTwoja odpowiedź: {userAnswer}");
-        if (userAnswer == correctAnswer) { Console.Write("Przyznano dostęp"); isSober = true; }
-        else Console.Write("Odmówiono dostępu");
-        Thread.Sleep(1500);
-        Console.Clear();
-        return isSober;
-    }
+            return isSober;
+    } 
 }
